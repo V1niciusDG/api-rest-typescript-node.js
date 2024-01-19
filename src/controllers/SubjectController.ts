0fb3384fc1b997f4aa76a07a84d12bfdde9cea0f
Vinicius Dalanora <vinicius.dalanora@waterservicestech.com>
2024-01-19 14:59:35 -0300

    repository

--- src/controllers/SubjectController.ts
+++ src/controllers/SubjectController.ts
@@ -1,16 +1,23 @@
 import { Request, Response } from 'express';
+import { subjectRepository } from '../repositories/subjectRepository';
 
 export class SubjectController {
   async create(req: Request, res: Response) {
     const { name } = req.body;
 
     if (!name) {
-      return res.json('nome obrigatório');
+      return res.status(400).json({ message: 'nome obrigatório' });
     }
 
     try {
+      const newSubject = subjectRepository.create({ name });
+
+      await subjectRepository.save(newSubject);
+
+      return res.status(200).json(newSubject);
     } catch (error) {
       console.error(error);
+
       return res.status(500).json({ message: 'Internal Server Error' });
     }
   }
