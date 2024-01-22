0f69e1aa693746b47b74bb3eea56af7838eadaeb
Vinicius Dalanora <vinicius.dalanora@waterservicestech.com>
2024-01-22 12:39:33 -0300

    adding relationships between classes and videos

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { RoomRepository } from '../repositories/roomRepository';
+import { VideoRepository } from '../repositories/videoRepository';
 
 export class RoomController {
   async create(req: Request, res: Response) {
@@ -10,6 +11,31 @@ export class RoomController {
       await RoomRepository.save(newRoom);
 
       return res.status(200).json(newRoom);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  }
+
+  async createVideo(req: Request, res: Response) {
+    const { title, url } = req.body;
+    const { idRoom } = req.params;
+
+    try {
+      const room = await RoomRepository.findOneBy({ id: Number(idRoom) });
+
+      if (!room) {
+        return res.status(404).json({ message: 'Aula não existe' });
+      }
+
+      const newVideo = VideoRepository.create({
+        title,
+        url,
+        room,
+      });
+      await VideoRepository.save(newVideo);
+
+      return res.status(201).json(newVideo);
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: 'Internal Server Error' });
